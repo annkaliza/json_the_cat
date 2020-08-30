c54dbc826fb0eae4b7071afa08ae01d74ab6c6eb
Kaliza Anna <annkaliza@gmail.com>
2020-08-30 11:44:37 +0000

    added a test function

--- breedFetcher.js
+++ breedFetcher.js
@@ -3,13 +3,20 @@ const request = require("request");
 
 const search = params[2];
 
-request(
-  `https://api.thecatapi.com/v1/breeds/search?q=${search}`,
-  (error, response, body) => {
-    if (error) {
-      console.log("error:", error);
+
+const fetchBreedDescription = function(breedName, callback) {
+
+  request(
+    `https://api.thecatapi.com/v1/breeds/search?q=${search}`,
+    (error, response, body) => {
+      if (error) {
+        console.log("error:", error);
+      }
+      const data = JSON.parse(body);
+      console.log(data);
     }
-    const data = JSON.parse(body);
-    console.log(data);
-  }
-);
+  );
+
+};
+
+module.exports = { fetchBreedDescription };
